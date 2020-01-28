4571b4b8542be99073209d466fe45d5f2cd3bf63
Annabelle Zhang <qiaoyan1@ualberta.ca>
2020-01-28 14:47:37 -0700

    finish validate command

--- shell379.c
+++ shell379.c
@@ -9,17 +9,30 @@
 #define MAX_LENGTH 20
 #define MAX_PT_ENTRIES 32
 
-bool readCmd() {
-    char cmd[MAX_ARGS][MAX_LENGTH];
-    char line[LINE_LENGTH+2]; // one space for new line, one space for \0
+int validateCmd(char line[LINE_LENGTH+2], char cmd[MAX_ARGS][MAX_LENGTH]) {
     int i = 0, ch = 0, arg = 0;
-    printf("SHELL379: ");
-    fgets(line, sizeof line, stdin);	
     int lineLen = strlen(line);
-    if (line[lineLen-1] != '\n') {
-        return false;
+    // find the position of &
+    char *p;
+    int idx;
+    p = strchr(line, '&');
+    idx = (int)(p - line);
+    // check if & is the last argument
+    if (p != NULL) {
+        if (lineLen-2 != idx) {
+            printf("Invalid Command: & must be the last argument.\n");
+            return -1;
+        } else if (line[lineLen-3] != ' ') {
+            printf("Invalid Command: & must be the last argument.\n");
+            return -1;
+        }
     }
-
+    // check if the input line ends with a newline to see 
+    // if it exceeds the maximum number of characters
+    if (line[lineLen-1] != '\n') {
+        printf("Invalid Command: the number of characters in the input line is more than 100.\n");
+        return -1;
+    }
     // find the first character
     int idxOfFirstChar = 0;
     while (line[idxOfFirstChar] == ' ') {
@@ -31,7 +44,8 @@ bool readCmd() {
             cmd[arg][ch]='\0';
             arg++;
             if (arg > MAX_ARGS) {
-                return false;
+                printf("Invalid Command: the number of arguments in the command is more than 7.\n");
+                return -1;
             }
             ch = 0;
             while (line[i+1] == ' ') {
@@ -42,29 +56,61 @@ bool readCmd() {
             if (line[i-1] != ' '){
                 arg++;
                 if (arg > MAX_ARGS) {
-                    return false;
+                    printf("Invalid Command: the number of arguments in the command is more than 7.\n");
+                    return -1;
                 }
             }
         } else {
             cmd[arg][ch] = line[i];
             ch++;
             if (ch > MAX_LENGTH) {
-                return false;
+                printf("Invalid Command: the number of characters in an argument is more than 20.\n");
+                return -1;
             }
         }
     }
-    int numOfArg = arg;
-    printf("numOfArg: %d\n\n", numOfArg);
-    for (i = 0; i < numOfArg; i++) {
-        printf("arg %d: %s\n", i+1, cmd[i]);
-    }
-    return true;   
+    return arg;   
 }
+
+void jobs() {
+    printf("jobs\n");
+}
+
 
 int main(int argc, char *argv[]) {
     bool exit = false;
     while (!exit) {
-        exit = readCmd();
+        char line[LINE_LENGTH+2]; // one space for new line, one space for \0
+        char cmd[MAX_ARGS][MAX_LENGTH];
+        int numOfArg = 0;
+
+        fflush(stdin);
+        printf("SHELL379: ");
+        fgets(line, sizeof line, stdin);
+
+        int ret = validateCmd(line, cmd);
+        if (ret == -1) {
+            continue;
+        } else {
+            numOfArg = ret;
+        }
+
+        // printf("numOfArg: %d\n", numOfArg);
+        // for (int i = 0; i < numOfArg; i++) {
+        //     printf("arg %d: %s\n", i+1, cmd[i]);
+        // }
+
+        // command jobs is entered
+        if (strcmp(cmd[0],"jobs") == 0) {
+            if (numOfArg != 1) {
+                printf("Invalid Command: jobs does not take any argument.\n");
+                continue;
+            } else {
+                jobs();
+            }
+        }
+
+
     }
     return 0;
 }
